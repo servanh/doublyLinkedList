1f95657217efcb0d2d93693fa87c878714fa44bd
servanh <servandobob95@gmail.com>
2017-10-02 21:02:32 -0500

    Program fully complete

--- src/QueueMain.java
+++ src/QueueMain.java
@@ -6,11 +6,15 @@ public class QueueMain {
     public static void main(String[] args) {
         QueueImpl q = new QueueImpl();
         System.out.println("Enter your input file name: ");
+
         Scanner scan = new Scanner(System.in);
+        String input = scan.next();
 
+        System.out.println();
         System.out.println("Input: ");
-        q.parseInput(scan.next());
+        q.parseInput(input);
 
+        System.out.println();
         System.out.println("Output: ");
         q.runInstructions();
         q.displayQueue();
--- src/StackMain.java
+++ src/StackMain.java
@@ -5,7 +5,15 @@ public class StackMain {
     public static void main(String[] args) {
         StackImpl s = new StackImpl();
         Scanner scan = new Scanner(System.in);
-        s.parseInput(scan.next());
+        System.out.println("Enter your input file name: ");
+        String input = scan.next();
+        System.out.println();
+        System.out.println("Input: ");
+        s.parseInput(input);
+
+        System.out.println();
+        System.out.println("Output: ");
+
         s.runInstructions();
         s.viewStack();
 
